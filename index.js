9f2216fd493aa780e5400ea3a915168018c6c3e2
David Nash <hisownspace@gmail.com>
2024-06-18 15:43:50 -0400

    create basic logic for updating orderbook

---
+++ index.js
@@ -0,0 +1,83 @@
+const { Centrifuge } = require("centrifuge");
+const { WebSocket } = require("ws");
+
+// live
+token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0MDAwMDAwMDAwIiwiZXhwIjo2NTQ4NDg3NTY5fQ.o_qBZltZdDHBH3zHPQkcRhVBQCtejIuyq8V1yj5kYq8";
+socket = "wss://api.prod.rabbitx.io/ws";
+// test
+// token =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwIiwiZXhwIjo1MjYyNjUyMDEwfQ.x_245iYDEvTTbraw1gt4jmFRFfgMJb-GJ-hsU9HuDik";
+// socket = "wss://api.testnet.rabbitx.io/ws";
+
+const orderbook = {
+  bids: [],
+  asks: [],
+  sequence: null,
+  timestamp: null,
+  bidsLen: 0,
+  asksLen: 0,
+};
+
+const centrifuge = new Centrifuge(socket, {
+  debug: true,
+  websocket: WebSocket,
+});
+centrifuge.setToken(token);
+
+const sub = centrifuge.newSubscription("orderbook:BTC-USD");
+
+const addToOrderBookNew = (ctx, group) => {
+  for (let trans of ctx.data[group]) {
+    const idx = orderbook[group].findIndex(
+      (el) => parseFloat(el[0]) === parseFloat(trans[0]),
+    );
+    if (trans[1] === "0") {
+      removeIdx >= 0 ? orderbook[group].splice(removeIdx, 1) : null;
+      orderbook[`${group}Len`] -= 1;
+      continue;
+    }
+    if (idx === -1) {
+      const insertIdx = orderbook[group].findIndex(
+        (el) => parseFloat(el[0]) > parseFloat(trans[0]),
+      );
+      orderbook[group].splice(insertIdx, 0, trans);
+      orderbook[`${group}Len`] += 1;
+    } else {
+      orderbook[group][idx] = trans;
+    }
+  }
+};
+
+const checkIntegrity = (ctx) => {
+  const prevSeq = orderbook.sequence;
+  const newSeq = ctx.data.sequence;
+  if (prevSeq + 1 != newSeq) {
+    throw new Error("Missed push!");
+  }
+  orderbook.timestamp = ctx.data.timestamp;
+  orderbook.sequence = newSeq;
+  if (newSeq % 100 === 0) {
+    console.log(orderbook);
+  }
+};
+
+sub.on("subscribed", (ctx) => {
+  orderbook.bids = ctx.data.bids;
+  orderbook.asks = ctx.data.asks;
+  orderbook.sequence = ctx.data.sequence;
+  orderbook.timestamp = ctx.data.timestamp;
+  orderbook.bidsLen = orderbook.bids.length;
+  orderbook.asksLen = orderbook.asks.length;
+  console.log(orderbook);
+});
+
+sub.on("publication", (ctx) => {
+  checkIntegrity(ctx);
+  addToOrderBookNew(ctx, "asks");
+  addToOrderBookNew(ctx, "bids");
+});
+
+sub.subscribe();
+
+centrifuge.connect();
